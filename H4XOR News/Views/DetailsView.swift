a1885c55ac93c92a7ca5d51b893c3b150a9f4b3c
Yarden Katz <yardenk@balink.net>
2021-04-07 09:09:49 +0300

    some minor refactoring

--- H4XOR News/Views/DetailsView.swift
+++ H4XOR News/Views/DetailsView.swift
@@ -6,7 +6,7 @@
 //
 
 import SwiftUI
-import WebKit
+
 struct DetailsView: View {
     
     let url: String?
@@ -19,24 +19,5 @@ struct DetailsView: View {
 struct DetailsView_Previews: PreviewProvider {
     static var previews: some View {
         DetailsView(url: "https://www.google.com")
-    }
-}
-
-struct WebView: UIViewRepresentable {
-    
-    let urlString: String?
-    
-    func makeUIView(context: Context) -> WebView.UIViewType {
-        return WKWebView()
-    }
-    
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        if let safeString = urlString {
-            if let url = URL(string: safeString) {
-                let request = URLRequest(url: url)
-                uiView.load(request)
-                
-            }
-        }
     }
 }
